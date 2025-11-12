03768520fbf7a3771ef24f4e163da4a3450b6033
RobbyR98 <robbyrunge@yahoo.com>
2025-11-12 20:05:03 +0100

    fix: ensure proper formatting in admin.py by adding a newline at the end of the file

--- orders_app/admin.py
+++ orders_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Order
 
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('get_customer_name', 'id', 'status', 'created_at')
